9414301110cf8592e12388a3600888aaab22eabd
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-03-14 22:01:11 +0900

    int(rand() * 10)

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 DIR=/srv/who-is-on && \
 . ${DIR}/env/production && \
 /usr/bin/racket ${DIR}/who-is-on-update.rkt >> ${DIR}/update.log 2>&1 && \
-at -f ${DIR}/update.sh now +`awk 'BEGIN{ srand(); print 55 + substr(rand(),3) % 10}'`minutes
+at -f ${DIR}/update.sh now +`awk 'BEGIN{srand(); print int(rand() * 10)}'`minutes
