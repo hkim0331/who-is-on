25e2cba333b83a6620b98fd80086a8509ab59d7d
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-05-31 10:18:16 +0900

    display todays start time

--- who-is-on-app.rkt
+++ who-is-on-app.rkt
@@ -28,7 +28,7 @@
          "weekday.rkt"
          "arp.rkt")
 
-(define VERSION "0.17.5.2")
+(define VERSION "0.18")
 
 (define sql3 (sqlite3-connect #:database (or (getenv "WIO_DB") "who-is-on.sqlite3")))
 
