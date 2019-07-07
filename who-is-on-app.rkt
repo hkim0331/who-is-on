e8ee8ff136be23440ff9c01ca26b92958b4c4877
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-07-07 22:16:47 +0900

    forgot bump version up to 0.18.2

--- who-is-on-app.rkt
+++ who-is-on-app.rkt
@@ -30,7 +30,7 @@
          "weekday.rkt"
          "arp.rkt")
 
-(define VERSION "0.18.1")
+(define VERSION "0.18.2")
 
 (define sql3 (sqlite3-connect #:database (or (getenv "WIO_DB") "who-is-on.sqlite3")))
 
