6ed4edf5f58b5c0b9c70760de2877bb02ea7ac48
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2019-09-19 13:24:34 +0900

    update README.md

--- README.md
+++ README.md
@@ -65,6 +65,7 @@ who-is-on.service はその systemd ファイル。
 
 ### FIXME/TODO
 
+* 野良アップ 'list'=>'last 30d'
 * [0.19] 1day100 へのリンク。2019-08-02
 
 * auth を独立した関数・モジュールに。
--- who-is-on-app.rkt
+++ who-is-on-app.rkt
@@ -347,7 +347,7 @@ where users.name=$1" user)))
          (displayln
           "<p>
 <a href='/i-m-here' class='btn btn-outline-primary btn-sm'>いるよ 😀</a>
-<a href='/list' class='btn btn-outline-primary btn-sm'>list</a>
+<a href='/list' class='btn btn-outline-primary btn-sm'>last month</a>
 <a href='/amend' class='btn btn-primary btn-sm'>amend</a>
 <a href='/users/new' class='btn btn-primary btn-sm'>add</a>
 </p>"))))))
